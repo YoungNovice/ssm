26287ed4cd5d6ff9ee2e28aa4170ddbaf385b867
YoungNovice <2306915539@qq.com>
2018-04-09 00:30:42 +0800

    配置事物工厂生成自定义事物管理器 给工厂配置一个别名

---
+++ src/main/java/com/learn/ssm/chapter3/transaction/MyJdbcTransaction.java
@@ -0,0 +1,54 @@
+package com.learn.ssm.chapter3.transaction;
+
+import org.apache.ibatis.session.TransactionIsolationLevel;
+import org.apache.ibatis.transaction.Transaction;
+import org.apache.ibatis.transaction.jdbc.JdbcTransaction;
+import org.apache.log4j.Logger;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MyJdbcTransaction extends JdbcTransaction implements Transaction{
+
+    private Logger logger = Logger.getLogger(MyJdbcTransaction.class);
+
+
+    public MyJdbcTransaction(DataSource ds, TransactionIsolationLevel desiredLevel, boolean desiredAutoCommit) {
+        super(ds, desiredLevel, desiredAutoCommit);
+    }
+
+    public MyJdbcTransaction(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        logger.info("my transaction getConnection");
+        return super.getConnection();
+    }
+
+    @Override
+    public void commit() throws SQLException {
+        logger.info("my transaction commit");
+        super.commit();
+    }
+
+    @Override
+    public void rollback() throws SQLException {
+        logger.info("my transaction getConnection");
+        super.rollback();
+    }
+
+    @Override
+    public void close() throws SQLException {
+        logger.info("my transaction close");
+        super.close();
+    }
+
+    @Override
+    public Integer getTimeout() throws SQLException {
+        logger.info("my transaction getTimeout");
+        return super.getTimeout();
+    }
+}
---
+++ src/main/java/com/learn/ssm/chapter3/transaction/MyTransactionFactory.java
@@ -0,0 +1,28 @@
+package com.learn.ssm.chapter3.transaction;
+
+import org.apache.ibatis.session.TransactionIsolationLevel;
+import org.apache.ibatis.transaction.Transaction;
+import org.apache.ibatis.transaction.TransactionFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class MyTransactionFactory implements TransactionFactory {
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+
+    @Override
+    public Transaction newTransaction(Connection connection) {
+        return new MyJdbcTransaction(connection);
+    }
+
+    @Override
+    public Transaction newTransaction(DataSource dataSource,
+          TransactionIsolationLevel transactionIsolationLevel, boolean b) {
+        return new MyJdbcTransaction(dataSource, transactionIsolationLevel, b);
+    }
+}
