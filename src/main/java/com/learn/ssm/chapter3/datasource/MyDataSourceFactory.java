e3f276aae47079bae5ed8fc1b1169d3f2a000b75
YoungNovice <2306915539@qq.com>
2018-04-09 09:07:11 +0800

    使用外部数据源

---
+++ src/main/java/com/learn/ssm/chapter3/datasource/MyDataSourceFactory.java
@@ -0,0 +1,34 @@
+package com.learn.ssm.chapter3.datasource;
+
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+import org.apache.ibatis.datasource.DataSourceFactory;
+import org.apache.log4j.Logger;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by Young on 2018/4/9.
+ */
+public class MyDataSourceFactory implements DataSourceFactory {
+
+    private Properties properties;
+
+    Logger logger = Logger.getLogger(MyDataSourceFactory.class);
+
+    @Override
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        try {
+            logger.info("使用dbcp数据源工厂");
+            return BasicDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
